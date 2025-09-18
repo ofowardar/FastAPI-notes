d04de743cbb83a065416c68683f3d13e206c9866
Omer Faruk Ozvardar <omerfarukozvardar@gmail.com>
2025-09-18 16:31:33 +0300

    Finish notes

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class Urun(BaseModel):
     fiyat: float                 # Bir şablon oluşturduk.
     stok: int
     kategori: Union[str,None] = None
+    stoktavar: Optional[bool] = None
 
 
 
@@ -48,3 +49,25 @@ def urun(urun:Urun):
     urun_dict.update({"stoktavar": urun.stok > 0})
     return {"output": urun_dict}
 
+
+
+# Request kodları anlamları
+
+"""
+100 Başlangıç
+200 OK İstek başarılı veri döndü
+201 Created Yeni kaynak oluşturuldu demek.
+204 No Context İşlem başarılı ama veri dönmedi?
+301 Moved Permently URL kalıcı olarak taşındı.
+302 Found Geçici yönlendirme.
+400 Bad Request Geçersiz istek yapıldı.
+401 Unauthorized Yetkilendirme gerekiyor.
+403 Forbidden Erişim izni yok 
+404 Not Found kaynak bulunamadı.
+409 Conflict Çakışma var.
+429 Too mant Requests Çok fazla istek yapıldı.
+ 
+
+
+
+"""
